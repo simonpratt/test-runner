be16c49f98753f6178b4a7c7bd675a30ac2646d7
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2023-04-12 07:40:26 +0930

    Tweaked env vars

--- test-runner-orchestrator/src/core/environment.ts
+++ test-runner-orchestrator/src/core/environment.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 const envSchema = z.object({
   UNIVERSAL_TEST_RUNNER_API_URL: z.string(),
   SERVER_PORT: z.coerce.number(),
-  MAX_CONCURRENT_TASKS: z.coerce.number(),
   RABBITMQ_JOB_EXCHANGE: z.string(),
   KUBERNETES_CONFIG_BASE64: z.string(),
   KUBERNETES_NAMESPACE: z.string(),
