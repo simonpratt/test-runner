dd435a0a7aeb4e0405459f5c94c408f041cada31
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2023-04-15 09:14:53 +0930

    Helper text for environment modal

--- test-runner-client/src/components/environments/EnvironmentForm.tsx
+++ test-runner-client/src/components/environments/EnvironmentForm.tsx
@@ -20,9 +20,24 @@ const EnvironmentForm = ({ value, onChange, onSubmit, isSaving }: EnvironmentFor
   return (
     <Form value={value} onChange={onChange} onSubmit={onSubmit}>
       <ControlGroup variation='comfortable'>
-        <Input name='id' label='Id' placeholder='environment-01' />
-        <Input name='name' label='Name' placeholder='Environment 01' />
-        <Input name='concurrencyLimit' label='Concurrency Limit' placeholder='4' />
+        <Input
+          name='id'
+          label='Id'
+          placeholder='environment-01'
+          description='A unique ID for the environment\nThis would be used by any external service that is requesting to start a test run'
+        />
+        <Input
+          name='name'
+          label='Name'
+          placeholder='Environment 01'
+          description='User facing name of the environment for when a test run is being started'
+        />
+        <Input
+          name='concurrencyLimit'
+          label='Concurrency Limit'
+          placeholder='4'
+          description='How many tests should be run in parallel against this environment?'
+        />
         <TextArea name='variables' label='Variables' placeholder='API_URL: http://xyz.com' />
         <Button type='submit' loading={isSaving}>
           Save
